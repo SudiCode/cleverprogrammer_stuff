cfeadff9f21f09a72462a2e2c398e1d2a5cd9b9e
SUDHIR KUMAR MISHRA <81106645+SudiCode@users.noreply.github.com>
2021-07-11 14:27:26 +0530

    Update tuples.py

--- Learn_Python/tuples.py
+++ Learn_Python/tuples.py
@@ -1,6 +1,6 @@
-groceries = ['apricot', 'banana', 'apple']
+groceries = ['apricot', 'banana', 'apple', 'mango']
 
-social_security_numbers = ('45645654', '234972348', '643785653874')  # tuple
+social_security_numbers = ('45645654', '234972348', '643785653874', '564327890765')  # tuple
 
 groceries.append('cheese')
 print(groceries)

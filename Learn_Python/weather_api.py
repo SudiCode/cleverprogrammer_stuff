f5d30a4c8e86397a7d4167a8cf31d37a7e742951
SUDHIR KUMAR MISHRA <81106645+SudiCode@users.noreply.github.com>
2021-07-11 12:30:23 +0530

    Update weather_api.py

--- Learn_Python/weather_api.py
+++ Learn_Python/weather_api.py
@@ -2,10 +2,10 @@ from pprint import pprint
 import requests
 
 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=289d79262f138bd6ebb226285fd277e3')
+s = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=289d79262f138bd6ebb226285fd277e3')
 
-#print(r.json())
-# pprint(r.json())
+#print(s.json())
+# pprint(s.json())
 
 
-print(r.json()['base'])
+print(s.json()['base'])
